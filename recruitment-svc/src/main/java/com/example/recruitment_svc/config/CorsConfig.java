5332e4148be6140a2aa95d0451e165b59a9d8534
amrita-arun <amrita.arun2013@gmail.com>
2025-10-12 22:46:21 -0700

    update corsconfig.java

--- recruitment-svc/src/main/java/com/example/recruitment_svc/config/CorsConfig.java
+++ recruitment-svc/src/main/java/com/example/recruitment_svc/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override public void addCorsMappings(CorsRegistry r) {
         r.addMapping("/api/**")
                 .allowedOrigins("http://localhost:5173", "http://localhost:3000")
-                .allowedMethods("GET","POST");
+                .allowedMethods("GET","POST","PATCH");
     }
 }
 
